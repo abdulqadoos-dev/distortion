24a1dd36c8fd7b7413a707a62e17b288a8195048
abdul qadoos <abdulqadoos.work@gmail.com>
2022-09-11 18:15:19 +0500

    HomePage Slider

--- components/header/logo/logo.tsx
+++ components/header/logo/logo.tsx
@@ -1,11 +1,11 @@
 import styles from './logo.module.scss'
 
 const Logo = (): JSX.Element => {
-  return (
-    <div className={styles.logo}>
-      <p>distortion</p>
-    </div>
-  )
+    return (
+        <div className={styles.logo}>
+            <img src="/images/distortion.svg"/>
+        </div>
+    )
 }
 
 export default Logo
