313e14c7f606aa67dd2549818cc2109409f01557
abdul qadoos <abdulqadoos.work@gmail.com>
2022-09-14 01:19:00 +0500

    footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,46 @@
 import styles from './footer.module.scss'
+import useTranslation from "next-translate/useTranslation";
+import Link from 'next/link'
 
 const Footer = () => {
-  return (
+    const {t, lang} = useTranslation('common')
+
+    return (
     <>
-      <footer className={styles.footer}></footer>
+      <footer className={styles.footer}>
+
+          <div className={styles.logo}>
+              <img src="/images/distortion.svg" />
+          </div>
+
+          <ul className={styles.navigation}>
+              <li>
+                  <Link href="/">
+                      <a>{t('about us')}</a>
+                  </Link>
+              </li>
+              <li>
+                    <Link href="/">
+                      <a>{t('contact us')}</a>
+                  </Link>
+              </li>
+              <li>
+                  <Link href="/">
+                      <a>{t('news')}</a>
+                  </Link>
+              </li>
+              <li>
+                  <Link href="/">
+                      <a>{t('terms & conditions')}</a>
+                  </Link>
+              </li>
+              <li>
+                  <Link href="/">
+                      <a>{t('privacy policy')}</a>
+                  </Link>
+              </li>
+          </ul>
+      </footer>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,29 @@ import UpcomingTournaments from '../components/home/upcoming'
 import FAQ from '../components/home/faq'
 import styles from '../styles/Home.module.css'
 import LeaderBoard from "../components/home/leaderBoard";
+import Copyrights from "../components/footer/copyrights";
 
 const Home: NextPage = () => {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Distortion</title>
-                <meta name="description" content="Distortion Dashboard"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+        <>
+            <div className={styles.container}>
+                <Head>
+                    <title>Distortion</title>
+                    <meta name="description" content="Distortion Dashboard"/>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
 
-            <Layout>
-                <main>
-                    <Slide/>
-                    <UpcomingTournaments/>
-                    <LeaderBoard/>
-                    <FAQ/>
-                </main>
-            </Layout>
-        </div>
+                <Layout>
+                    <main>
+                        <Slide/>
+                        <UpcomingTournaments/>
+                        <LeaderBoard/>
+                        <FAQ/>
+                    </main>
+                </Layout>
+            </div>
+            <Copyrights/>
+        </>
     )
 }
 
