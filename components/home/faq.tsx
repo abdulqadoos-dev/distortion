7e01ba3fd14695226c53106a76ff8696994dffc6
abdul qadoos <abdulqadoos.work@gmail.com>
2022-10-06 22:46:50 +0500

    type fixed

--- components/home/faq.tsx
+++ components/home/faq.tsx
@@ -2,10 +2,11 @@ import styles from './faq.module.scss'
 import useTranslation from "next-translate/useTranslation";
 import {useState} from "react";
 
-const FAQ = () => {
+const FAQ = () : JSX.Element => {
     const {t, lang} = useTranslation('common')
 
-    const [activeTab, setActiveTab] = useState(null)
+    const [activeTab, setActiveTab] = useState<number>(0)
+
 
     return (
         <>
@@ -14,7 +15,7 @@ const FAQ = () => {
 
                 <div className={styles.faqsListItems}>
 
-                    <div className={styles.listItem} onClick={() => activeTab === 1 ? setActiveTab(null) : setActiveTab(1)}>
+                    <div className={styles.listItem} onClick={() => activeTab === 1 ? setActiveTab(0) : setActiveTab(1)}>
                         <div className={`${styles.listItemHeader}  ${activeTab === 1 &&  styles.listItemHeaderActive}`}>
                             <p>What is Distortion ?</p>
                             <img src="/images/icons/chevron-up.svg" alt=""/>
@@ -27,7 +28,7 @@ const FAQ = () => {
                         </div>
                     </div>
 
-                    <div className={styles.listItem} onClick={() => activeTab === 2 ? setActiveTab(null) : setActiveTab(2)}>
+                    <div className={styles.listItem} onClick={() => activeTab === 2 ? setActiveTab(0) : setActiveTab(2)}>
                         <div className={`${styles.listItemHeader}  ${activeTab === 2 &&  styles.listItemHeaderActive}`}>
                             <p>What is a non custodial wallet ?</p>
                             <img src="/images/icons/chevron-up.svg" alt=""/>
@@ -37,7 +38,7 @@ const FAQ = () => {
                                 itaque libero nulla porro quos similique, unde voluptates.</p>
                         </div>
                     </div>
-                    <div className={styles.listItem}  onClick={() => activeTab === 3 ? setActiveTab(null) : setActiveTab(3)}>
+                    <div className={styles.listItem}  onClick={() => activeTab === 3 ? setActiveTab(0) : setActiveTab(3)}>
                         <div className={`${styles.listItemHeader}  ${activeTab === 3 &&  styles.listItemHeaderActive}`}>
                             <p>How to send funds on my wallet?</p>
                             <img src="/images/icons/chevron-up.svg" alt=""/>
@@ -47,7 +48,7 @@ const FAQ = () => {
                                 itaque libero nulla porro quos similique, unde voluptates.</p>
                         </div>
                     </div>
-                    <div className={styles.listItem}  onClick={() => activeTab === 4 ? setActiveTab(null) : setActiveTab(4)}>
+                    <div className={styles.listItem}  onClick={() => activeTab === 4 ? setActiveTab(0) : setActiveTab(4)}>
                         <div className={`${styles.listItemHeader}  ${activeTab === 4 &&  styles.listItemHeaderActive}`}>
                             <p>How to participate to a tournament?</p>
                             <img src="/images/icons/chevron-up.svg" alt=""/>
@@ -57,7 +58,7 @@ const FAQ = () => {
                                 itaque libero nulla porro quos similique, unde voluptates.</p>
                         </div>
                     </div>
-                    <div className={styles.listItem}  onClick={() => activeTab === 5 ? setActiveTab(null) : setActiveTab(5)}>
+                    <div className={styles.listItem}  onClick={() => activeTab === 5 ? setActiveTab(0) : setActiveTab(5)}>
                         <div className={`${styles.listItemHeader}  ${activeTab === 5 &&  styles.listItemHeaderActive}`}>
                             <p>How to claim my rewards?</p>
                             <img src="/images/icons/chevron-up.svg" alt=""/>
@@ -75,4 +76,4 @@ const FAQ = () => {
     )
 }
 
-export default FAQ
+export default FAQ();
